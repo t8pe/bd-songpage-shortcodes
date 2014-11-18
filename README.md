280be79a619e67609e4e0327c2d2aca5c0350413
Ben Deschamps <ben@t8pe.com>
2014-11-18 16:27:56 -0500

    Plugin functional & ready to get CSS work done.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ bd-songpage-shortcodes
 ======================
 
 A plugin to simplify making nice shop pages in WooCommerce
+
+Still to be done:
+- icons instead of link text
+- graceful failure modes
--- bd-songpage-shortcodes.php
+++ bd-songpage-shortcodes.php
@@ -9,6 +9,13 @@ Author URI: http://bendeschamps.com
 License: GPLv2
 */
 
+function bd_songpage_scripts() {
+	wp_enqueue_style( 'bd-songpage-css', plugins_url( 'bd-songpage-shortcodes/css/style.css' ) );
+}
+
+add_action( 'wp_enqueue_scripts', 'bd_songpage_scripts' );
+
+
 
 function bdAlbumShortcode( $slug, $enclosure = null ) {
 
@@ -30,10 +37,10 @@ $album_img = '<img src="' . $image . '" width="100px" height="100px" />';
 // These are the various bits of the return string, chopped into variables. I can do this more elegantly later I think.
 $album_opening = '<div class="hd-album">';
 $album_thumbnail = '<div class="hd-album-thumbnail">';
-$album_title = '</div><div class="hd-album-title"></div>';
-$album_description = '<div class="hd-album-text">';
-$album_buy_alls = '</div><div class="hd-buy-alls"><div class="hd-buy-all-mp3s">';
-$album_buy_cd = '</div><div class="hd-buy-CD">';
+$album_title = '</div><div class="hd-album-title">';
+$album_description = '</div><div class="hd-album-text">';
+$album_buy_alls = '</div><div class="hd-album-buy-alls"><div class="hd-album-buy-all-mp3s">';
+$album_buy_cd = '</div><div class="hd-album-buy-CD">';
 $album_end = '</div></div>';
 
 // This does the real work of returning the shortcode.
@@ -51,17 +58,17 @@ function bdSongShortcode( $songID ) {
   $product = new WC_product( $song ); 
 
 // These are the various bits of the return string, chopped into variables. I can do this more elegantly later I think.
-  $mp3j_info = '<div class="hd-album-individual-song"><div class="album-song-mp3j">';
-  $song_title = '</div><div class="album-song-title">';
-  $price = '</div><div class="album-song-price"> $';
-  $buy_now = '</div><div class="album-song-buynow">';
-  $more_info = '>Buy Now Link</a></div><div class="album-song-moreinfo"><a href="';
+  $mp3j_info = '<div class="hd-album-individual-song">'; // stripped out <div class="album-song-mp3j">
+  $song_title = '<div class="hd-album-song-title">'; // stripped out </div> 
+  $price = '</div><div class="hd-album-song-price"> $';
+  $buy_now = '</div><div class="hd-album-song-buynow">';
+  $more_info = '>Buy Now Link</a></div><div class="hd-album-song-moreinfo"><a href="';
   $end_string = 'More Info</a></div></div>';
-  
-  // Still having trouble with the MP3Jplayer appearing at the top of the page for whatever fucking reason.
+    
+// Still having trouble with the MP3Jplayer appearing at the top of the page for whatever fucking reason. But I can prob float it w CSS.
 
 // This does the real work of returning the shortcode.
-  return $mp3j_info . mp3j_put( '[mp3j track="' . get_post_meta( $song, "mp3ee", true ) . '"]' ) . $song_title . get_the_title( $song ) . $price . $product->regular_price . $buy_now . '<a href=' . get_site_url() . '?add-to-cart=' .  $song . $more_info . get_site_url() . "/song_wiki/" . get_the_title( $song ) . '">' . $end_string;
+  return $mp3j_info .  mp3j_put('[mp3j track="' . get_post_meta( $song, "mp3ee", true ) . '" title="" style="bigger2"]' ) . $song_title . get_the_title( $song ) . $price . $product->regular_price . $buy_now . '<a href=' . get_site_url() . '?add-to-cart=' .  $song . $more_info . get_site_url() . "/song_wiki/" . get_the_title( $song ) . '">' . $end_string;
 }
 
 add_shortcode( 'Song', 'bdSongShortcode' );
