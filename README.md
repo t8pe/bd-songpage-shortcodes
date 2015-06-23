4eb899c41714fcd68efe6dbd92abad82399d708e
Ben Deschamps <ben@t8pe.com>
2015-06-23 10:48:01 -0400

    Updated Merch shortcode with Description. Removed lorem text. Should change this so it's no longer the lorem version, I suppose.

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ bd-songpage-shortcodes
 
 A plugin to simplify making nice shop pages in WooCommerce
 
-THIS VERSION (loremversion, 0.2) actually does most of what I want for
+THIS VERSION (loremversion, 0.4) actually does most of what I want for
 the new HD.com.
 
-Still to be done:
-- icons instead of link text
-- graceful failure modes
+New features:
+- Merch shortcode, with Product Description
+(Advanced->Attributes->'product')
+- removed lorem text from Albums as it's unnecessary (for now, we'll
+see how Heather likes that)
--- bd-songpage-shortcodes.php
+++ bd-songpage-shortcodes.php
@@ -3,7 +3,7 @@
 Plugin Name: Ben's Song-Page Shortcodes
 Plugin URI: http://bendeschamps.com
 Description: Plugin to enable Album & Song shortcodes in WordPress without Woo stupidity
-Version: 0.2
+Version: 0.4
 Author: Ben Deschamps
 Author URI: http://bendeschamps.com
 License: GPLv2
@@ -54,7 +54,7 @@ function bdAlbumShortcode( $slug, $enclosure = null ) {
   $album_buy_cd = '</div><div class="hd-album-buy-CD">';
   $album_more_info = '</div><div class="hd-album-moreinfo"><a href="' . get_site_url() . '/album_wiki/#' . $albuminfo->slug . '"><img src="' . ICON_PATH . INFO_ICON . '" /></a>';
   $album_end = '</div></div></div><ul class="songlist">';
-  $lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
 
 // This does the real work of returning the shortcode.
 return 
@@ -64,7 +64,7 @@ $album_opening
 . $album_title 
 . $albuminfo->name 
 . $album_description 
-. $lorem  
+  
 
 //. $albuminfo->description 
 . $album_buy_alls 
@@ -88,9 +88,6 @@ function bdMerchShortcode( $merchID ) {
 
   $image_link  	= wp_get_attachment_url( get_post_thumbnail_id( $merch ) );
 
-  
-  /* $image = get_the_post_thumbnail( $product->ID, 'thumbnail' ); */
-  /* $image_link = wp_get_attachment_url( $image ); */
   $merch_img = '<img src="' . $image_link . '" width="140px" height="140px" />';
   
    // These are the various bits of the return string, chopped into variables.
@@ -101,8 +98,9 @@ function bdMerchShortcode( $merchID ) {
   $buy_now = '</div><div class="hd-merch-buynow">';
   $buylink = '<a href="' . get_site_url() . '?add-to-cart=' . $product->id . '"><img src="' . ICON_PATH . CART_ICON . '" /></a></div>';
   $more_info = '<div class="hd-merch-moreinfo"><a href="';
-  $end_string = '<img src="' . ICON_PATH . INFO_ICON . '" /></a></div></div>';  
-  $lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+  $end_string = '<img src="' . ICON_PATH . INFO_ICON . '" /></a></div></div><br>';  
+  $product_description = $product->get_attribute( 'description' ); // The description must be included as an Attribute in Advanced
+  
 // This does the real work of returning the shortcode.
 
   return
@@ -115,54 +113,12 @@ function bdMerchShortcode( $merchID ) {
     . $product->regular_price
     . $buy_now   
     . $buylink
-    . $lorem
+    . $product_description
     . $more_info . get_site_url() . "/merch_wiki/#" . get_the_title( $merch ) . '">'
     . $end_string;
   
 }
-  //  $merchslug = $slug[0]; // because $slug is an array not an object
-  //  $merchinfo = get_term_by( 'slug', $merchslug, 'product_cat' ); 
  
-/* // These extract the IDs of the products in question & construct buy-now links for them. */
-/*   $getMerch = get_page_by_title( $merchinfo->name .  OBJECT, 'product' ); // took out " (physical CD)" */
-/*   $merchbuylink = '<a href="' . get_site_url() . '?add-to-cart=' . $getMerch->ID . '"> <img src="' . ICON_PATH . BUY_MERCH_ICON . '" /></a>'; */
-
-/* // This sets up the thumbnail. I can change the size of it as desired. */
-  /* $thumbnail_id = get_woocommerce_term_meta( $merchinfo->term_id, 'thumbnail_id', true ); */
-  /* $image = wp_get_attachment_url( $thumbnail_id ); */
-  /* $merch_img = '<img src="' . $image . '" width="140px" height="140px" />'; */
-
-/* // These are the various bits of the return string, chopped into variables. */
-/*   $merch_opening = '<div class="hd-merch-container"><div class="hd-merch">';  */
-/*   $merch_thumbnail = '<div class="hd-merch-thumbnail">'; */
-/*   $merch_title = '</div><div class="hd-merch-title">'; */
-/*   $merch_description = '</div><div class="hd-merch-text">'; */
-/*   $merch_buy_alls = '</div><div class="hd-merch-buy-alls"><div class="hd-merch-buy-all-mp3s">'; */
-/*   $merch_buy_cd = '</div><div class="hd-merch-buy-merch">'; */
-/*   $merch_more_info = '</div><div class="hd-merch-moreinfo"><a href="' . get_site_url() . '/merch_wiki/#' . $merchinfo->slug . '"><img src="' . ICON_PATH . INFO_ICON . '" /></a>'; */
-/*   $merch_end = '</div></div></div><ul class="songlist">'; */
-
-// This does the real work of returning the shortcode.
-/* return  */
-/* $merch_opening  */
-/* . $merch_thumbnail */
-/* . $merch_img  */
-/* . $merch_title  */
-/* . $merchinfo->name  */
-/* . $merch_description  */
-/* . $lorem   */
-
-/* //. $merchinfo->description  */
-/* . $merch_buy_alls  */
-/* . $merch_more_info */
-/*   //. $MP3buylink  */
-/*   //. $merch_buy_cd  */
-/* . $merchbuylink */
-/* . $merch_end  */
-/* . do_shortcode ( $enclosure )  */
-/* . "</ul></div>"; */
- 
-/* } */
 
 add_shortcode( 'Merch', 'bdMerchShortcode' );
 
